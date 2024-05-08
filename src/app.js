7beab4493bc0f60bb008153e01739369232a20ee
cpgravina <74896127+cpgravina@users.noreply.github.com>
2024-05-08 11:42:44 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["My boss", "My colleague", "My neighbor", "The bird"];
+  let action = ["changed", "borrowed", "painted", "stored"];
+  let what = ["my pencil", "my sandwich", "the notebook", "the meeting notes"];
+  let when = ["yesterday", "this morning", "last week", "during the meeting"];
+
+  let whorandom = Math.floor(Math.random() * who.length);
+  let actionrandom = Math.floor(Math.random() * action.length);
+  let whatrandom = Math.floor(Math.random() * what.length);
+  let whenrandom = Math.floor(Math.random() * when.length);
+
+  let excusewho = who[whorandom];
+  let excuseaction = action[actionrandom];
+  let excusewhat = what[whatrandom];
+  let excusewhen = when[whenrandom];
+
+  let resultado =
+    excusewho + " " + excuseaction + " " + excusewhat + " " + excusewhen;
+  document.getElementById("excuse").innerHTML = resultado;
 };
